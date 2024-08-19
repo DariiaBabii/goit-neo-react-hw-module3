7012affb2363af7089319603059dcf66d63aa35f
dariia_babii <dariia.babii02@gmail.com>
2024-08-19 14:29:02 +0200

    contacts form

---
+++ src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import { FaUser, FaPhoneAlt } from 'react-icons/fa';
+
+import classes from './Contact.module.css';
+
+function Contact({ contact, onDelete }) {
+    return (
+        <div className={classes['contact']}>
+            <div className={classes['contact-info']}>
+                <FaUser />
+                <span className={classes['className']}>{contact.name}</span>
+                <FaPhoneAlt />
+                <span className={classes['classNumber']}>{contact.number}</span>
+            </div>
+            <button onClick={() => onDelete(contact)} className={classes['contact-delete']} type='button'>Delete</button>
+        </div>
+
+    );
+}
+
+export default Contact;
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,22 @@
+import { useId } from "react";
+
+import classes from "./SearchBox.module.css";
+
+function SearchBox({ value, onChange }) {
+  const searchId = useId();
+
+  return (
+    <div className={classes["search-box"]}>
+      <label htmlFor={searchId}>Find contacts by name</label>
+      <input
+        id={searchId}
+        value={value}
+        type="search"
+        className={classes["search-box-input"]}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </div>
+  );
+}
+
+export default SearchBox;
